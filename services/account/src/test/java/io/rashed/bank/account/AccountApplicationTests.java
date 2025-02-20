39ee65574e7dc79e7dc3ab536d92acb127477e5c
Rashed Maaitah <bahran568@gmail.com>
2025-02-20 22:08:29 +0300

    init account microservice setup

---
+++ services/account/src/test/java/io/rashed/bank/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rashed.bank.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
