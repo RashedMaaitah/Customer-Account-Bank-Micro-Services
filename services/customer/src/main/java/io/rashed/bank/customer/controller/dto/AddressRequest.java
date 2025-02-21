63eac89be3bf7633ed805d45b36a9e5201ec1cb1
Rashed Maaitah <bahran568@gmail.com>
2025-02-21 16:03:09 +0300

    <refactor> better validation message for AddressRequest

--- services/customer/src/main/java/io/rashed/bank/customer/controller/dto/AddressRequest.java
+++ services/customer/src/main/java/io/rashed/bank/customer/controller/dto/AddressRequest.java
@@ -2,22 +2,27 @@ package io.rashed.bank.customer.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record AddressRequest(
-        @NotBlank
+        @NotBlank(message = "Country cannot be blank")
+        @Size(max = 50, message = "Country name cannot exceed 50 characters")
         @JsonProperty
         String country,
 
-        @NotBlank
+        @NotBlank(message = "City cannot be blank")
+        @Size(max = 50, message = "City name cannot exceed 50 characters")
         @JsonProperty
         String city,
 
-        @NotBlank
-        @JsonProperty(value = "street_name")
+        @NotBlank(message = "Street name cannot be blank")
+        @Size(max = 100, message = "Street name cannot exceed 100 characters")
+        @JsonProperty("street_name")
         String street,
 
-        @NotBlank
-        @JsonProperty(value = "zip_code")
+        @NotBlank(message = "ZIP code cannot be blank")
+        @Size(min = 4, max = 10, message = "ZIP code must be between 4 and 10 characters")
+        @JsonProperty("zip_code")
         String zipCode
 ) {
 }
